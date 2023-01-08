fedf12824551b800478f55e1d19ef2d1dc45955c
soumyaKaddouri <soumayakad76@gmail.com>
2023-01-08 13:33:48 +0100

    adding Contact & Folder classes

---
+++ Models/Contact.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactManager.Models
+{
+    internal class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Company { get; set; }
+        public ContactLink Link { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime ModificationDate { get; set; }
+
+        public Folder ParentFolder { get; set; }
+
+        public Contact()
+        {
+            CreationDate = DateTime.Now;
+            ModificationDate = DateTime.Now;
+        }
+
+    }
+}
---
+++ Models/Folder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactManager.Models
+{
+    internal class Folder
+    {
+        public string Name { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime ModificationDate { get; set; }
+        public List<Contact> Contacts { get; set; }
+
+        public Folder()
+        {
+            Contacts = new List<Contact>();
+        }
+    }
+}
