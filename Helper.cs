ae365f36feb1917a092c0cded7d2c8aa6eb4561b
soumyaKaddouri <soumayakad76@gmail.com>
2023-01-14 13:45:10 +0100

    Add some modifications to: Socle applicatif

--- Helper.cs
+++ Helper.cs
@@ -1,6 +1,7 @@
 ﻿using ContactManager.Models;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,79 +10,72 @@ namespace ContactManager
 {
     public static class Helper
     {
-        public static Folder AddContact(Folder currentFolder)
+        static TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+        public static Folder AddContact(Folder currentFolder, string[] fields)
         {
-            Console.WriteLine("Enter the first name: ");
-            var firstName = Console.ReadLine();
-            Console.WriteLine("Enter the last name: ");
-            var lastName = Console.ReadLine();
-            Console.WriteLine("Entrez the email: ");
-            var email = Console.ReadLine();
-
             // Vérification de la validité de l'adresse email
             try
             {
-                var addr = new System.Net.Mail.MailAddress(email);
+                var addr = new System.Net.Mail.MailAddress(fields[3]);
             }
             catch (FormatException)
             {
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("Invalid email address.");
+                Console.ResetColor();
                 return currentFolder;
             }
 
-            Console.WriteLine("Enter the company name:");
-            var company = Console.ReadLine();
-
-            Console.WriteLine("Enter the link (Friend, Colleague, Relation, Network):");
-            var link = Console.ReadLine();
-
-            string[] links = new string[4] { "Friend", "Colleague", "Relation", "Network" };
+            string[] links = new string[4] { "friend", "colleague", "relation", "network" };
             ContactLink _link;
 
             //Link Verification
-            if (links.Contains(link))
+            if (links.Contains(fields[5]))
             {
                 //Convertion de string vers le type enum ContactLink
-                _link = (ContactLink)Enum.Parse(typeof(ContactLink), link);
+                
+                _link = (ContactLink)Enum.Parse(typeof(ContactLink), textInfo.ToTitleCase(fields[5]));
             }
             else
             {
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("Invalid Link format (must be: Friend, Colleague, Relation, or Network)");
+                Console.ResetColor();
+
                 return currentFolder;
             }
 
 
-            if (firstName != null && lastName != null && company != null)
-            {
-                //Creation de nouveau contact
-                var newContact = new Contact(firstName, lastName, email, company, _link);
+            //Creation de nouveau contact
 
-                // Ajout de nouveau contact au dossier courant
-                currentFolder.Contacts.Add(newContact);
-                Console.WriteLine("Contact added successfully.");
-            }
-            else { Console.WriteLine("The first name, last name or company must not be null "); }
+            /*var newContact = new Contact(fields[1].Substring(0,1).ToUpper() + fields[1].Substring(1),
+                fields[2].Substring(0, 1).ToUpper() + fields[2].Substring(1), fields[3],
+                fields[4].Substring(0,1).ToUpper() + fields[4].Substring(1), _link);*/
 
+            var newContact = new Contact(textInfo.ToTitleCase(fields[1]),textInfo.ToTitleCase(fields[2]), fields[3],
+               textInfo.ToTitleCase(fields[4]), _link);
+
+            // Ajout de nouveau contact au dossier courant
+            currentFolder.Contacts.Add(newContact);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Contact added successfully.");
+            Console.ResetColor ();
+           
             return currentFolder;
         }
 
-        public static Folder CreateFolder(Folder currentFolder)
+        public static Folder CreateFolder(Folder currentFolder, string nameFolder)
         {
-            Console.WriteLine("Entrez the folder name:");
-            var name = Console.ReadLine();
+            var newFolder = new Folder(textInfo.ToTitleCase(nameFolder));
 
-            if(name != null)
-            {
-                var newFolder = new Folder(name);
+            currentFolder.ChildFolders.Add(newFolder);
+            currentFolder = newFolder;
 
-                currentFolder.ChildFolders.Add(newFolder);
-                currentFolder = newFolder;
-                Console.WriteLine("Folder created successfully.");
-            }
-            else
-            {
-                Console.WriteLine("The name folder is required");
-            }
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Folder created successfully.");
+            Console.ResetColor () ;
+           
 
             return currentFolder;
 
@@ -90,14 +84,14 @@ namespace ContactManager
 
         public static void DisplayStructure(Folder folder, int depth)
         {
-            Console.WriteLine(new string('-', depth) + folder.Name);
+            Console.WriteLine(new string('-', depth) + folder.ToString());
             foreach (var subFolder in folder.ChildFolders)
             {
                 DisplayStructure(subFolder, depth + 1);
             }
             foreach (var contact in folder.Contacts)
             {
-                Console.WriteLine(new string('-', depth + 1) + contact.FirstName + " " + contact.LastName);
+                Console.WriteLine(new string('-', depth + 1) + contact.ToString());
             }
         }
     }
--- Program.cs
+++ Program.cs
@@ -15,27 +15,60 @@ namespace ContactManager
         {
             while (true)
             {
-                Console.WriteLine("1. View the entire structure");
-                Console.WriteLine("2. Create a new folder");
-                Console.WriteLine("3. Add a contact");
-                Console.WriteLine("4. Quit");
+                Console.ResetColor();
+                Console.WriteLine(@"|*-----------------Menu-------------------*|");
+                Console.WriteLine(@"|* 1. View the entire structure           *|");
+                Console.WriteLine(@"|* 2. Create a new folder                 *|");
+                Console.WriteLine(@"|* 3. Add a contact                       *|");
+                Console.WriteLine(@"|* 4. Quit                                *|");
+                Console.WriteLine(@"|*----------------------------------------*|");
+
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write("> ");
 
                 var input = Console.ReadLine();
-                switch (input)
+                Console.ResetColor();
+
+                input = (!string.IsNullOrEmpty(input) ?input.ToLower() : string.Empty);
+
+                string[] fields = input.Split(' ', StringSplitOptions.None);
+
+                switch (fields[0])
                 {
                     case "1":
                         Helper.DisplayStructure(root, 0);
                         break;
                     case "2":
-                        currentFolder =  Helper.CreateFolder(currentFolder);
+                        if (fields.Length > 1)
+                        {
+                            currentFolder = Helper.CreateFolder(currentFolder, fields[1]);
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.WriteLine("Please mention the folder name");
+                            Console.ResetColor();
+                        }
                         break;
                     case "3":
-                        currentFolder =  Helper.AddContact(currentFolder);
+                        if (fields.Length > 5)
+                        {
+                            currentFolder = Helper.AddContact(currentFolder, fields);
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.WriteLine("Please add all the contact information");
+                            Console.ResetColor();
+                        }
                         break;
                     case "4":
                         return;
                     default:
+                        Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine("Unknown instruction");
+                        Console.ResetColor();
                         break;
                         
                 }
